6d4a25a8989ec7e04eb1e421cc2ce28c151500fa
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2022-11-07 00:40:20 +0700

    Update pertemuan3.sh

--- labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan3/code/pertemuan3.sh
+++ labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan3/code/pertemuan3.sh
@@ -1,1 +1,30 @@
-
+touch pertemuan3
+nano pertemuan3
+~
+~
+~
+echo "=================================" 
+echo "        BIODATA MAHASISWA        "
+echo "================================="
+echo -n "Masukan Nama		:"
+read nama
+echo -n "Masukan NPM		:"
+read npm
+echo -n "Masukan Kelas		:"
+read kelas
+echo -n "Masukan Nilai UTS	:"
+read uts
+echo -n "Masukan Nilai UAS	:"
+read uas
+let nilai=($uts+$uas)/2
+echo "---------------------------------"
+echo "Nama	: " $nama
+echo "NPM		: " $npm
+echo "Kelas	: " $kelas
+echo "Nilai	: " $nilai
+echo "---------------------------------"
+~
+~
+~
+chmod +x pertemuan3
+./pertemuan3

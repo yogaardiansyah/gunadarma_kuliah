490cecf94a50509d5aa7ae9ff8d0fb82e4d0a809
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2022-11-09 21:59:00 +0700

    Update pertemuan4-2.sh

--- labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan4/code/pertemuan4-2.sh
+++ labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan4/code/pertemuan4-2.sh
@@ -1,1 +1,30 @@
+clear
+echo "==========================================";
+echo "          MENU RESTAURANT YOGA            ";
+echo "------------------------------------------";
+echo "          1. Pagpag khas Filiphina        ";
+echo "          2. Jhal Muri khas Bangladesh    ";
+echo "          3. Exit                         ";
+echo "------------------------------------------";
+read -p "Masukan pilihan [1-3]: " pil;
+echo "------------------------------------------" 
+case $pil in
+1)
+                echo -n "Berapa mangkuk porsi Pagpag? "
+        read jum
+        let bayar=jum4000;
+;;
+2)
 
+        echo -n "Berapa porsi porsi Jhal Muri? "
+        read jum
+        let bayar=jum3000;
+;;
+*)
+        echo "Sampai Jumpa"
+        exit
+;;
+esac
+
+echo "Anda harus membayar = Rp $bayar"
+echo "Terima kasih sudah berkunjung"

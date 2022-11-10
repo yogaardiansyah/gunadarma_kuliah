7e38c014a90e263754353142041428323b4891f7
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2022-11-10 08:11:01 +0700

    Update pertemuan4-la.sh

--- labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan4/code/pertemuan4-la.sh
+++ labti/tingkat1/semester1/Peng_Tekno_Komp_Inf_1/pertemuan4/code/pertemuan4-la.sh
@@ -1,4 +1,5 @@
 # Yoga Ardiansyah 51422643 1IA25
+# Laporan Akhir Praktikum 4
 clear
 echo "************************************"
 echo "  MENU RESTAURANT YOGA ARDIASNYAH"
@@ -10,38 +11,38 @@ echo "   4. Buku Tulis"
 echo "   5. Exit"
 echo "************************************"
 echo -n "Masukkan Pilihan [1-5] : "
-read pilihan
+read pil
 echo "************************************"
-if [ $pilihan -eq 1 ];
+if [ $pil -eq 1 ];
 then
-    echo -n "Berapa Porsi Bakso ? "
-    read porsi
-    let total=$porsi*3000
-    echo "Anda harus membayar = Rp" $total
+    echo -n "Beli berapa pensil ? "
+    read tpens
+    let totalpens=$tpens*2500
+    echo "Anda harus membayar = Rp" $totalpens
     echo "Terima kasih sudah berkunjung"
     echo "--------------------------------"
-elif [ $pilihan -eq 2 ];
+elif [ $pil -eq 2 ];
 then
-    echo -n "Berapa Porsi Gado-Gado ? "
-    read porsi
-    let total=$porsi*4000
-    echo "Anda harus membayar = Rp" $total
+    echo -n "Beli berapa penghapus ? "
+    read tpeng
+    let totalpeng=$tpeng*1000
+    echo "Anda harus membayar = Rp" $totalpeng
     echo "Terima kasih sudah berkunjung"
     echo "--------------------------------"
-    elif [ $pilihan -eq 3 ];
+elif [ $pil -eq 3 ];
 then
-    echo -n "Berapa Porsi Gado-Gado ? "
-    read porsi
-    let total=$porsi*4000
-    echo "Anda harus membayar = Rp" $total
+    echo -n "Beli berapa penggaris ? "
+    read tpenga
+    let totalpenga=$tpenga*3000
+    echo "Anda harus membayar = Rp" $totalpenga
     echo "Terima kasih sudah berkunjung"
     echo "--------------------------------"
-    elif [ $pilihan -eq 4 ];
+elif [ $pil -eq 4 ];
 then
-    echo -n "Berapa Porsi Gado-Gado ? "
-    read porsi
-    let total=$porsi*4000
-    echo "Anda harus membayar = Rp" $total
+    echo -n "Beli berapa buku ? "
+    read tbuk
+    let totalbuk=$tbuk*5000
+    echo "Anda harus membayar = Rp" $totalbuk
     echo "Terima kasih sudah berkunjung"
     echo "--------------------------------"
 else 

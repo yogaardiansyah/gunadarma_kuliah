c6a0f59595ee6f9494836c56ee60998919695b32
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2022-11-17 17:49:54 +0700

    Update ajc_cac.java

--- selain-lain/ngoding/tugas_java/ajc_cac.java
+++ selain-lain/ngoding/tugas_java/ajc_cac.java
@@ -1,3 +1,4 @@
+// Sample Code
 public class Test {
 
 	public static void main(String[] args){
@@ -5,4 +6,58 @@ public class Test {
 		System.out.println("Hello, World!");
 	
 	}
+}
+
+// Java code for thread creation by implementing
+// the Runnable Interface
+class MultithreadingDemo implements Runnable {
+    public void run()
+    {
+        try {
+            // Displaying the thread that is running
+            System.out.println(
+                "Thread " + Thread.currentThread().getId()
+                + " is running");
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
+ 
+// Main Class
+class Multithread {
+    public static void main(String[] args)
+    {
+        int n = 8; // Number of threads
+        for (int i = 0; i < n; i++) {
+            Thread object
+                = new Thread(new MultithreadingDemo());
+            object.start();
+        }
+    }
+}
+
+// Example Sub Class
+class Vehicle {
+  protected String brand = "Ford";        // Vehicle attribute
+  public void honk() {                    // Vehicle method
+    System.out.println("Tuut, tuut!");
+  }
+}
+
+class Car extends Vehicle {
+  private String modelName = "Mustang";    // Car attribute
+  public static void main(String[] args) {
+
+    // Create a myCar object
+    Car myCar = new Car();
+
+    // Call the honk() method (from the Vehicle class) on the myCar object
+    myCar.honk();
+
+    // Display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class
+    System.out.println(myCar.brand + " " + myCar.modelName);
+  }
 }

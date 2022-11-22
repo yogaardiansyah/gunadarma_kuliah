8b20bcc624061af62290d9b1b29fca6afea26b58
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2022-11-22 09:14:07 +0700

    Add files via upload

---
+++ lepkom/tingkat1/semester1/Fund_Desk/pertemuan4/code/ConsoleApp1/Program.vb
@@ -0,0 +1,24 @@
+'Listing 1 Pert4 Fundamental Desktop'
+'Yoga Ardiansyah 51422643'
+Imports System
+
+Module Module1
+    Sub Main()
+        Dim npm, nama, alamat As String
+
+        Console.Write("Masukan NPM Anda    = ")
+        npm = Console.ReadLine()
+        Console.Write("Masukan Nama Anda   = ")
+        nama = Console.ReadLine()
+        Console.Write("Masukan Alamat Anda = ")
+        alamat = Console.ReadLine()
+        Console.WriteLine("========================================")
+        Console.WriteLine("NPM    : " & npm)
+        Console.WriteLine("Nama   : " & nama)
+        Console.WriteLine("Alamat : " & alamat)
+        Console.Write("============================================")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
